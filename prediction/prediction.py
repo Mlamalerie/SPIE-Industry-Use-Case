b8fca5a03fda9e26f37e89f4bad3c72577d49706
Wael382 <mael.trevin@gmail.com>
2023-01-19 22:45:10 +0100

    script prediction fonctionnel

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,8 +1,10 @@
+from typing import Dict
+import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 from data.consommation import ConsommationDataManager
 
-def time_series(logement_name: str, consommation_manager: ConsommationDataManager = ConsommationDataManager()) -> ...:
+def time_series(logement_name: str, consommation_manager: ConsommationDataManager = ConsommationDataManager()) -> Dict[str, pd.Series]:
 
     df_logement = consommation_manager.get_df_conso_by_logement_name(logement_name)
     series_logement = {}
@@ -10,19 +12,24 @@ def time_series(logement_name: str, consommation_manager: ConsommationDataManage
         series_logement[column] = df_logement[column]
     return series_logement
 
-def solo_prediction_hw(time_serie: ...) -> ...:
+def solo_prediction_hw(time_serie: pd.Series) -> pd.Series:
 
-    hw = ExponentialSmoothing(time_serie, trend = 'add', damped_trend = True,
-                              seasonal = 'mul', seasonal_periods = 365).fit()
+    hw = ExponentialSmoothing(time_serie, trend = 'add', damped_trend = True, seasonal = 'mul', seasonal_periods = 365).fit()
     return hw.forecast()
 
-def all_predictions_hw(time_series: ...) -> ...:
+def total_prediction_hw(time_series: Dict[str, pd.Series]) -> pd.Series:
+
+    return solo_prediction_hw(time_series['consommation'])
+
+def time_predictions_hw(time_series: Dict[str, pd.Series]) -> Dict[str, pd.Series]:
 
     predictions = {}
     for key in time_series:
-        predictions[key] = solo_prediction_hw(time_series[key])
+        if key != 'consommation':
+            predictions[key] = solo_prediction_hw(time_series[key])
     return predictions
 
-def total_prediction_hw(time_series: ...) -> ...:
 
-    return solo_prediction_hw(time_series['consommation'])
+
+
+
